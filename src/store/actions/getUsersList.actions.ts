5d7fc47af3b37840dcab35ca60bdd5bfaff4f9d3
FelipeRuiz94 <felipe.r.p.1994@gmail.com>
2021-08-26 16:16:42 +0200

    Remove not needed actions

--- src/store/actions/getUsersList.actions.ts
+++ src/store/actions/getUsersList.actions.ts
@@ -4,7 +4,6 @@ import { User } from "../../model/user";
 export const getUsersListActionTypes = {
   GET_USERS_LIST_REQUEST: "GET_USERS_LIST_REQUEST",
   GET_USERS_LIST_SUCCESS: "GET_USERS_LIST_SUCCESS",
-  GET_USERS_LIST_ERROR: "GET_USERS_LIST_ERROR",
 };
 
 export const getUsersListRequestAction = (): AnyAction => ({
@@ -15,9 +14,4 @@ export const getUsersListRequestAction = (): AnyAction => ({
 export const getUsersListSucessAction = (usersList: User[]): AnyAction => ({
   type: getUsersListActionTypes.GET_USERS_LIST_SUCCESS,
   payload: usersList,
-});
-
-export const getUsersListErrorAction = (error: string): AnyAction => ({
-  type: getUsersListActionTypes.GET_USERS_LIST_ERROR,
-  payload: error,
 });
