c51ac8d7b2e7d1ce964ebd65a0d135102dc449ed
FelipeRuiz94 <felipe.r.p.1994@gmail.com>
2021-08-26 13:35:18 +0200

    Add service for connect user

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -31,7 +31,31 @@ const addUser = (name: string): Promise<void> => {
     });
 };
 
+const connectUser = (data: {
+  user1Id: number;
+  user2Id: number;
+}): Promise<void> => {
+  const { user1Id, user2Id } = data;
+
+  const requestConfig = {
+    ...defaultRequestConfig,
+    method: "POST",
+    body: JSON.stringify({ user1Id, user2Id }),
+  };
+  console.log(data);
+  return fetch("api/users", requestConfig)
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error("Error connecting user");
+    })
+    .then((response) => response)
+    .catch((error) => {
+      throw new Error(error);
+    });
+};
+
 export const userService = {
   getUsersList,
   addUser,
+  connectUser,
 };
